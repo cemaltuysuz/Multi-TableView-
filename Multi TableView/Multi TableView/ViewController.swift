da5ab0a26c4fce83ef8bd01b0db495615ea4bec8
cemaltuysuz <ctuysuz18@posta.pau.edu.tr>
2021-10-11 19:00:52 +0300

    init project

---
+++ Multi TableView/Multi TableView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Multi TableView
+//
+//  Created by cemal tüysüz on 11.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
